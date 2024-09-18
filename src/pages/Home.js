7b337a51462e86278bdd06b1499f25dbf96dd9df
GuillermoRivero98 <141684687+GuillermoRivero98@users.noreply.github.com>
2024-09-18 16:40:57 -0300

    adaptacion
    agregar el modal al index.css y al home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
+import Modal from "../components/Modal";
 
 const Home = () => {
+
+    const[isModalOpen, setModalOpen] = useState(false);
+
+    const openModal = () => setModalOpen(true);
+    const closeModal = () => setModalOpen(false);
+
     return (
         <div>
             <h2>BInevenido a Mis Primeros Pasos</h2>
             <p>Este es el sitio principal con noticias y artículos sobre la educación en la primera Infancia</p>
+            <button onClick={openModal}>Abrir Modal</button>
+
+            <Modal isOpen={isModalOpen} onClose={closeModal}>
+                <h3>Este es el contenido del modal</h3>
+                <p>Información importante</p>
+            </Modal>
         </div>
     );
 }
