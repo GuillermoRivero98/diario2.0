1fce0bf5be9f513d1d32ab6d2079a2f1d7e0f877
GuillermoRivero98 <141684687+GuillermoRivero98@users.noreply.github.com>
2024-09-18 23:27:25 -0300

    fix getart

--- src/api/getArticles.js
+++ src/api/getArticles.js
@@ -2,10 +2,17 @@ const API_URL = "/api/articles";
 
 export async function getArticles() {
     try {
-        const response = await fetch(API_URL);
+        const response = await fetch(API_URL, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
         if (!response.ok) {
             throw new Error("Error al obtener los artículos");
         }
+
         return await response.json();
     } catch (error) {
         console.error(error);
